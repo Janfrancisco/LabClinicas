93ccd330c573180681bcc7158631a37758463b1a
Janfrancisco <janramalho.100@gmail.com>
2024-06-29 15:05:14 -0400

    fix some bugs

--- lab_clinicas_self_service/lib/src/modules/self_service/documents/scan/documents_scan_page.dart
+++ lab_clinicas_self_service/lib/src/modules/self_service/documents/scan/documents_scan_page.dart
@@ -1,7 +1,10 @@
+import 'package:asyncstate/asyncstate.dart';
 import 'package:camera/camera.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:fe_lab_clinicas_self_service_cb/src/modules/self_service/documents/widgets/document_box_widget.dart';
 import 'package:fe_lab_clinicas_self_service_cb/src/modules/self_service/widget/lab_clinicas_self_service_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
 
@@ -44,14 +47,14 @@ class _DocumentsScanPageState extends State<DocumentsScanPage> {
               children: [
                 Image.asset('assets/images/cam_icon.png'),
                 const SizedBox(
-                  height: 24,
+                  height: 16,
                 ),
                 const Text(
                   'TIRAR A FOTO AGORA',
                   style: LabClinicasTheme.titleSmallStyle,
                 ),
                 const SizedBox(
-                  height: 32,
+                  height: 20,
                 ),
                 const Text(
                   'Posicione o documento dentro do quadrado abaixo e aperte o botão para tirar a foto',
@@ -62,7 +65,7 @@ class _DocumentsScanPageState extends State<DocumentsScanPage> {
                       color: LabClinicasTheme.blueColor),
                 ),
                 const SizedBox(
-                  height: 24,
+                  height: 20,
                 ),
                 FutureBuilder(
                   future: cameraController.initialize(),
@@ -77,13 +80,48 @@ class _DocumentsScanPageState extends State<DocumentsScanPage> {
                         );
                       case AsyncSnapshot(connectionState: ConnectionState.done):
                         if (cameraController.value.isInitialized) {
-                          return CameraPreview(cameraController);
+                          return ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: SizedBox(
+                              width: sizeOf.width * .45,
+                              child: CameraPreview(
+                                child: DottedBorder(
+                                  dashPattern: const [1, 10, 1, 3],
+                                  radius: const Radius.circular(16),
+                                  borderType: BorderType.RRect,
+                                  strokeWidth: 4,
+                                  strokeCap: StrokeCap.square,
+                                  color: LabClinicasTheme.orangeColor,
+                                  child: const SizedBox.expand(),
+                                ),
+                                cameraController,
+                              ),
+                            ),
+                          );
                         }
                     }
                     return const Center(
                       child: Text('Erro ao carregar a câmera'),
                     );
                   },
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: sizeOf.width * .8,
+                  height: 48,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      final nav = Navigator.of(context);
+                      final foto =
+                          await cameraController.takePicture().asyncLoader();
+
+                      nav.pushNamed('/self-service/documents/scan/confirm',
+                          arguments: foto);
+                    },
+                    child: const Text('TIRAR FOTO'),
+                  ),
                 )
               ],
             ),
--- lab_clinicas_self_service/lib/src/modules/self_service/documents/scan_confirm/documents_scan_confirm_page.dart
+++ lab_clinicas_self_service/lib/src/modules/self_service/documents/scan_confirm/documents_scan_confirm_page.dart
@@ -1,15 +1,98 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:fe_lab_clinicas_self_service_cb/src/modules/self_service/documents/widgets/document_box_widget.dart';
+import 'package:fe_lab_clinicas_self_service_cb/src/modules/self_service/widget/lab_clinicas_self_service_appbar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lab_clinicas_core/lab_clinicas_core.dart';
 
 class DocumentsScanConfirmPage extends StatelessWidget {
   const DocumentsScanConfirmPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
+    final foto = ModalRoute.of(context)!.settings.arguments as XFile;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Documents Confirm'),
+      appBar: LabClinicasSelfServiceAppbar(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SingleChildScrollView(
+          child: Container(
+            width: sizeOf.width * .85,
+            margin: const EdgeInsets.only(top: 18),
+            padding: const EdgeInsets.all(32),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: LabClinicasTheme.orangeColor)),
+            child: Column(
+              children: [
+                Image.asset('assets/images/foto_confirm_icon.png'),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Text(
+                  'CONFIRA A SUA FOTO',
+                  style: LabClinicasTheme.titleSmallStyle,
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: SizedBox(
+                    width: sizeOf.width * .5,
+                    child: DottedBorder(
+                        dashPattern: const [1, 10, 1, 3],
+                        radius: const Radius.circular(16),
+                        borderType: BorderType.RRect,
+                        strokeWidth: 4,
+                        strokeCap: StrokeCap.square,
+                        color: LabClinicasTheme.orangeColor,
+                        child: Image.file(File(foto.path))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        width: sizeOf.width * .8,
+                        height: 48,
+                        child: OutlinedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('TIRAR OUTRA FOTO'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        height: 48,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('SALVAR'),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
